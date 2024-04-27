fbdc67c35feb3422a9341e7323c67c63e4425359
yu-matsumura <29217415+yuma1217@users.noreply.github.com>
2024-04-27 17:36:30 +0900

    onnxを読み込めるようにする

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import { useEffect } from "react";
 import * as ort from "onnxruntime-web";
 
 export default function Home() {
   useEffect(() => {
-    async function loadModel() {
-      // onnxモデルを読み込む
-      const loadedModel = await ort.InferenceSession.create(
-        "/model/model.onnx"
-      );
-    }
-  });
+    loadModel();
+  }, []);
   return (
     <div>
-      <h1>Home</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <h1>ONNX Example</h1>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
     </div>
   );
+}
+
+async function loadModel() {
+  // onnxモデルを読み込む
+  const loadedModel = await ort.InferenceSession.create("/mobilenetv2-7.onnx");
+  console.log(loadedModel);
 }
