f2875862cefb6a49e5090ee1d69b9ae6967a6cbd
yu-matsumura <29217415+yuma1217@users.noreply.github.com>
2024-04-27 16:39:12 +0900

    Add Onnxモデル

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
+import { useEffect } from "react";
+import * as ort from "onnxruntime-web";
 
 export default function Home() {
+  useEffect(() => {
+    async function loadModel() {
+      // onnxモデルを読み込む
+      const loadedModel = await ort.InferenceSession.create(
+        "/model/model.onnx"
+      );
+    }
+  });
   return (
     <div>
       <h1>Home</h1>
