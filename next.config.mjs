2c72622f90bac4b24446b05b837625b479e096f0
yu-matsumura <29217415+yuma1217@users.noreply.github.com>
2024-04-28 16:02:19 +0900

    Add onnxruntime-webをNextjsApprouterで使用できることを検証 (#1)
    * Init
    
    * Add Onnxモデル
    
    * onnxを読み込めるようにする
    
    * onnxを読み込んで使うところまで

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import CopyPlugin from "copy-webpack-plugin";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new NodePolyfillPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "./node_modules/onnxruntime-web/dist/ort-wasm.wasm",
+            to: "static/chunks",
+          },
+          {
+            from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm",
+            to: "static/chunks",
+          },
+          {
+            from: "./model",
+            to: "static/chunks",
+          },
+        ],
+      })
+    );
+
+    return config;
+  },
+  reactStrictMode: true,
+};
 
 export default nextConfig;
